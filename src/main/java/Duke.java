7cf545872c557a2d2967433c49c989a96147f22b
E0310898 <E0310898@u.nus.edu>
2019-08-27 00:38:09 +0800

    Did Level 4, The naming convention has yet to be changed/standardized as priority is to get the working/accurate code up before the stipulated deadlin! working well with some minor user_input problem handling will be improving this at Level 5

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,20 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 public class Duke {
-    public static void print_output(String n) //Used to print the output repeatedly!
+
+    private static void print_output(Task CurrentTask, int no_of_tasks) // Used to print the output repeatedly!
     {
         String user_output_dash = "_______________________________";
         System.out.println(user_output_dash);
-        System.out.println("Task added:" + " " + n);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(CurrentTask.toString());
+        System.out.println("Now you have " + no_of_tasks + " tasks in the list.");
         System.out.println(user_output_dash);
     }
 
     public static void main(String[] args) {
 
-        Scanner user_input = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         String greeting = "_______________________________\n"
                         + "Hello! I'm Duke\n"
                         + "What can I do for you?\n"
@@ -22,35 +25,36 @@ public class Duke {
         ArrayList<Task> listOfTasks = new ArrayList<Task>();
 
         String user_output_dash = "_______________________________"; // just a definition
-        String n = "not_bye";
+
         int iterate; int counter;
+        String user_input; //declared outside to be used by the do-while loop
 
-        while(!n.equals("bye"))
+        do
         {
-            n = user_input.nextLine();
+            user_input = scanner.nextLine();
 
-            if(n.equals("list"))
+            if(user_input.contains("list"))
             {
                 System.out.println(user_output_dash + "\n" + "Here are the tasks in your list:");
                 iterate = 0;
                 counter = 0;
-                System.out.println(user_output_dash);
-                while(counter < listOfTasks.size())
+                while(counter < listOfTasks.size()) // this prints out all the different tasks you have
                 {
-                    System.out.println((counter+=1) + "." + "[" + listOfTasks.get(iterate).getStatusIcon() + "]" + " " + listOfTasks.get(iterate).description);
+                    Task currentTask = listOfTasks.get(iterate);
+                    System.out.println((counter+=1) + "." + currentTask.toString());
                     iterate++;
                 }
                 System.out.println(user_output_dash);
             }
-            else if (n.matches("done \\d+")) {
-                int listIndex = Integer.parseInt(n.replaceAll("[^0-9]", ""));
+            else if (user_input.matches("done \\d+")) {
+                int listIndex = Integer.parseInt(user_input.replaceAll("[^0-9]", ""));
                 listIndex -= 1;
                 if (listIndex < listOfTasks.size()) {
                     Task currentTask = listOfTasks.get(listIndex); // object instance
                     currentTask.markAsDone();
                     System.out.println(user_output_dash);
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println("[" + listOfTasks.get(listIndex).getStatusIcon() + "]" + " " + listOfTasks.get(listIndex).description);
+                    System.out.println(currentTask.toString());
                     System.out.println(user_output_dash);
                 }
                 else
@@ -60,13 +64,36 @@ public class Duke {
                     System.out.println(user_output_dash);
                 }
             }
-            else if (!n.equals("bye"))
+            else if (user_input.contains("todo"))
             {
-                Task task = new Task(n);
-                listOfTasks.add(task);
-                print_output(n);
+                String todo_name = user_input.replace("todo ","");
+                Todo TodoTask = new Todo(todo_name);
+                listOfTasks.add(TodoTask);
+                print_output(TodoTask, listOfTasks.size());
             }
-        }
+            else if (user_input.contains("deadline"))
+            {
+                user_input = user_input.replace("deadline","");
+                String[] parts = user_input.split("/by");
+                String deadline_name = parts[0];
+                String deadline_date = parts[1];
+                Deadline DeadlineTask = new Deadline(deadline_name,deadline_date);
+                listOfTasks.add(DeadlineTask);
+                print_output(DeadlineTask,listOfTasks.size());
+            }
+            else if (user_input.contains("event"))
+            {
+                user_input = user_input.replace("event","");
+                String[] parts = user_input.split("/at");
+                String event_name = parts[0];
+                String event_dateandtime = parts[1];
+                Event EventTask = new Event(event_name,event_dateandtime);
+                listOfTasks.add(EventTask);
+                print_output(EventTask,listOfTasks.size());
+
+            }
+        }while (!user_input.equals("bye"));
+
         String goodbye = "_______________________________\n"
                        + "Bye. Hope to see you again soon!\n"
                        + "_______________________________\n";
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,4 @@
-public class Task {
+ class Task {
     protected String description; // basically similar to describing features of the class
     protected boolean isDone;
 
@@ -7,11 +7,12 @@ public class Task {
         this.isDone = false;
     }
 
-    public String getStatusIcon() {
+     public String getStatusIcon() {
         return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
     }
 
     public void markAsDone() {
         this.isDone = true;
     }
+
 }
