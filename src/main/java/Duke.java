bf3ffe3a1b56ae79043929568ffb5fd5009b6e89
E0310898 <E0310898@u.nus.edu>
2019-09-04 17:19:12 +0800

    Bug fix in main function and delete command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,34 +3,29 @@ import java.util.ArrayList;
 
 public class Duke {
 
-    private static String filePath = "D:/DukeDatabase/ArrayList";
+    private String filePath = "D:/DukeDatabase/ArrayList";
     private Storage storage;
     private TaskList tasks;
     private Ui ui;
     private File file = new File(filePath);
 
-    public static void main(String[] args) throws DukeException {
-        try{
-        new Duke(filePath).run();
-    }
-        catch(DukeException e){
-            e.getMessage();
-        }
+    public static void main(String[] args){
+        new Duke().run();
     }
 
-    public Duke(String filePath) throws DukeException {
+    public Duke(){
         ui = new Ui();
-        storage = new Storage(filePath,file);
+        storage = new Storage(this.filePath,file);
 
         try {
             tasks = new TaskList(storage.loadFile(file));
         }
         catch (DukeException e) {
-            throw new DukeException(DukeException.FILE_DOES_NOT_EXIST());
+            Ui.printMessage(e.getMessage());
         }
     }
 
-    public void run() throws DukeException {
+    public void run(){
         Ui.printGreeting();
         boolean isExit = false;
         do {
