477129d073dc5575371700b40f4568e0345de45b
E0310898 <E0310898@u.nus.edu>
2019-09-10 20:42:18 +0800

    Added JUnit tests

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class TaskTest {
+    @Test
+    public void dummytest(){
+        Assertions.assertEquals(2,2);
+    }
+}
